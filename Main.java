664c4bf16ec42fcbe24e953719ff59ba54404615
Villias <108465395+VilliasDev@users.noreply.github.com>
2023-04-23 12:50:26 +0200

    expanded_Question2

--- Main.java
+++ Main.java
@@ -4,10 +4,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Question 1: Why did I make this level?");
+        System.out.println("Question 1: Why did I make this Quiz?");
         String answer = scanner.nextLine();
         if (answer.equals("Because you were bored")) {
             System.out.println("Exactly.");
+            Question2.main(args);
         }
 
     }
---
+++ out/production/Quiz-I-was-bored/README.md
@@ -0,0 +1,6 @@
+# Quiz-I-was-bored
+Exactly </br>
+
+Language used: Java </br>
+
+Braincells used: all two of them </br>
