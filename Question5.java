6a0058b536596dd0a9850a88257afd0852279bb3
Villias <108465395+VilliasDev@users.noreply.github.com>
2023-04-23 17:24:24 +0200

    Added final Question

--- Question5.java
+++ Question5.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 public class Question5 {
     public static void main() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Why does this Quiz have only five Questions?");
+        String answer = scanner.nextLine();
+        if (answer.equals("You go bored again")) {
+            System.out.println("Exactly. Bye. Got bored again. Maybe I do it again in C");
+        } else {
+            System.out.println("Wrong.");
+            main();
+        }
     }
 }
